ca2acecbbde1bdad6f0aa79678a9984a9ee2ab61
N.Scommegna <N.Scommegna@AN-NSCOMMEGNA.mshome.net>
2022-02-03 18:04:15 +0100

    First commit

---
+++ workmanager/src/main/java/comworkmanager/WorkmanagerApplication.java
@@ -0,0 +1,13 @@
+package comworkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkmanagerApplication.class, args);
+	}
+
+}
---
+++ workmanager/src/test/java/comworkmanager/WorkmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package comworkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
